646983d8bab55306357781d1d07977dd8a084583
YunDongGeun <dong@geun.kr>
2024-11-06 00:31:17 +0900

    add insertValue.sql

---
+++ src/main/java/uniteProject/global/jdbc/insertValue.sql
@@ -0,0 +1,13 @@
+INSERT INTO dormitory (id, dorm_name, gender) VALUES (1, '오름', 'M');
+INSERT INTO dormitory (id, dorm_name, gender) VALUES (2, '오름', 'F');
+INSERT INTO dormitory (id, dorm_name, gender) VALUES (3, '푸름', 'M');
+INSERT INTO dormitory (id, dorm_name, gender) VALUES (4, '푸름', 'F');
+
+INSERT INTO fee_management (id, dorm_name, fee_type, amount) VALUES (1, '오름', 'ROOM_2', 628800);
+INSERT INTO fee_management (id, dorm_name, fee_type, amount) VALUES (2, '푸름', 'ROOM_2', 462000);
+INSERT INTO fee_management (id, dorm_name, fee_type, amount) VALUES (3, '푸름', 'ROOM_4', 385000);
+INSERT INTO fee_management (id, dorm_name, fee_type, amount) VALUES (4, '오름', 'MEAL_5', 716800);
+INSERT INTO fee_management (id, dorm_name, fee_type, amount) VALUES (5, '오름', 'MEAL_7', 575390);
+INSERT INTO fee_management (id, dorm_name, fee_type, amount) VALUES (6, '푸름', 'MEAL_5', 622720);
+INSERT INTO fee_management (id, dorm_name, fee_type, amount) VALUES (7, '푸름', 'MEAL_7', 503670);
+INSERT INTO fee_management (id, dorm_name, fee_type, amount) VALUES (8, '푸름', 'MEAL_0', 0);
